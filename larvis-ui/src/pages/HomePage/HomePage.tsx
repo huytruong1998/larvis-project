550bb20d2a3cdc1f38b58109ba49adc0b57db832
Huy Truong <skyzerzlol@gmail.com>
2025-03-29 03:21:18 +0200

    feat: mock list users

--- larvis-ui/src/pages/HomePage/HomePage.tsx
+++ larvis-ui/src/pages/HomePage/HomePage.tsx
@@ -4,13 +4,15 @@ import { Col, Layout, Row } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import { ProfileCard } from '@/components/ProfileCard/ProfileCard';
 import { TimeSeriesChart } from '@/components/TimeSeriesChart/TimeSeriesChart';
+import { UserList } from '@/components/ListUsers/UserList';
 
 export const HomePage: React.FC = () => {
   return (
     <Content style={{ padding: '24px', minHeight: '100vh', background: '#fff' }}>
       <Row align="bottom" justify="space-between" gutter={[16, 32]}>
         <Col xs={24} md={8}>
-          <ProfileCard />
+          {/* <ProfileCard /> */}
+          <UserList />
         </Col>
         <Col xs={24} md={16}>
           <TimeSeriesChart />
